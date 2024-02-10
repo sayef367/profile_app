58358afd8a44e0a49ddcedf6b41c9dba91455d3a
Md Sayeful Islam <sayefulislam367@gmail.com>
2024-02-11 00:19:14 +0600

    home UI and data fetch done

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,47 @@
+import Link from "next/link";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('https://dummyjson.com/users');
+  return res.json();
+};
+
+export default async function Home() {
+  const data = await getData();
+  const users = data.users;
+
+  if(!users) return <h2 className="text-center mt-5">Loading...</h2>;
+
   return (
-    <main>
-      <h1 className="text-center">Wellcome to Profile App</h1>
+    <main className="container">
+      {/* <h1 className="text-center mt-4 mb-4">Wellcome to Profile App</h1> */}
+      <div className="row mt-5">
+        <div className="input-group mb-5">
+          <input 
+            type="text"
+            className="form-control rounded-pill me-3 text-center" 
+            placeholder="Search User Name"
+          />
+          <button type="button" className="btn btn-dark rounded-pill">Add User</button>
+        </div>
+        {
+          users.map((user) => {
+            return(
+              <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={user.id}>
+                <Link href={'/'} 
+                  className="link-underline link-underline-opacity-0 link-body-emphasis card p-3">
+                  <p className="mb-0">
+                    Id: {user.id} <br />
+                    Name: {`${user.firstName} ${user.lastName}`} <br />
+                    Email: {user.email} <br />
+                    Address: {`${user.address.address}, ${user.address.city}`} <br />
+                    Company: {user.company.name}
+                  </p>
+                </Link>
+              </div>
+            )
+          })
+        }
+      </div>
     </main>
   );
 };
--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="pt-2 pb-1 bg-body-tertiary fw-light">
+    <footer className="pt-2 pb-1 mt-4 bg-body-tertiary fw-light">
       <p className="text-center pt-3">© 2024 All Rights Reserved. Developed by Md Sayeful Islam</p>
     </footer>
   );
